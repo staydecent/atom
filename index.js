2df88d7c5b1cfe1059f2733d8a05352750edf32b
Adrian Unger <dev@staydecent.ca>
2017-06-14 16:48:40 -0700

    Apply standardjs formatting.

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
 /* globals define */
-(function(root, factory) {
-  'use strict';
+(function (root, factory) {
+  'use strict'
   if (typeof define === 'function' && define.amd) {
-    define([], factory);
+    define([], factory)
   } else if (typeof exports === 'object') {
-    module.exports = factory();
+    module.exports = factory()
   } else {
-    root.atom = factory();
+    root.atom = factory()
   }
-}(this, function() {
-  'use strict';
+}(this, function () {
+  'use strict'
 
-  return function(reducer, initialState) {
-    var listeners = [];
-    var state = initialState;
+  return function (reducer, initialState) {
+    var listeners = []
+    var state = initialState
 
     return {
       dispatch: dispatch,
       subscribe: subscribe,
       getState: getState
-    };
+    }
 
-    function dispatch(action) {
-      var newState = reducer(action, state);
+    function dispatch (action) {
+      var newState = reducer(action, state)
 
       if (newState === undefined) {
-        throw new E('Reducer must return a value.');
+        throw new E('Reducer must return a value.')
       } else if (typeof newState.then === 'function') {
-        throw new E('Reducer cannot return a Promise.');
+        throw new E('Reducer cannot return a Promise.')
       } else if (typeof newState === 'function') {
-        newState(dispatch);
+        newState(dispatch)
       } else {
-        cb(newState);
+        cb(newState)
       }
     }
 
-    function subscribe(listener) {
+    function subscribe (listener) {
       if (typeof listener !== 'function') {
-        throw new E('listener must be a function');
+        throw new E('listener must be a function')
       }
-      listeners.push(listener);
+      listeners.push(listener)
     }
 
-    function getState() {
-      return state;
+    function getState () {
+      return state
     }
 
-    function cb(newState) {
-      state = newState;
-      for (var x=0; x < listeners.length; x++) {
-        listeners[x]();
+    function cb (newState) {
+      state = newState
+      for (var x = 0; x < listeners.length; x++) {
+        listeners[x]()
       }
     }
 
-    function E(message) {
-      this.message = message;
-      this.name = "AtomException";
-      this.toString = function() {
-        return this.name + ': ' + this.message;
-      };
+    function E (message) {
+      this.message = message
+      this.name = 'AtomException'
+      this.toString = function () {
+        return this.name + ': ' + this.message
+      }
     }
-  };
-}));
+  }
+}))
--- test/subscribe.js
+++ test/subscribe.js
@@ -1,51 +1,50 @@
-'use strict';
+'use strict'
 
-var atom = require('../');
-var test = require('tape');
+var atom = require('../')
+var test = require('tape')
 
-var initialState = 0;
-var genericAction = {a:1};
-var returnTrue = function() { return true; };
+var initialState = 0
+var genericAction = {a: 1}
+var returnTrue = function () { return true }
 
+test('subscribed listener should be called', function (t) {
+  t.plan(1)
 
-test('subscribed listener should be called', function(t) {
-  t.plan(1);
-  
-  var called = false;
-  var store = atom(returnTrue, initialState);
+  var called = false
+  var store = atom(returnTrue, initialState)
 
-  store.subscribe(function() {
-    called = true;
-  });
-  
-  store.dispatch(genericAction);
+  store.subscribe(function () {
+    called = true
+  })
 
-  t.ok(called);
-});
+  store.dispatch(genericAction)
 
-test('subscribed listener should get updated state', function(t) {
-  t.plan(1);
-  
-  var store = atom(returnTrue, initialState);
+  t.ok(called)
+})
 
-  store.subscribe(function() {
-    t.notEqual(initialState, store.getState());
-  });
-  
-  store.dispatch(genericAction);
-});
+test('subscribed listener should get updated state', function (t) {
+  t.plan(1)
 
-test('subscribe should throw when listener not a function', function(t) {
-  t.plan(1);
+  var store = atom(returnTrue, initialState)
 
-  var thrown = false;
-  var store = atom(returnTrue, initialState);
+  store.subscribe(function () {
+    t.notEqual(initialState, store.getState())
+  })
+
+  store.dispatch(genericAction)
+})
+
+test('subscribe should throw when listener not a function', function (t) {
+  t.plan(1)
+
+  var thrown = false
+  var store = atom(returnTrue, initialState)
 
   try {
-    store.subscribe({});
+    store.subscribe({})
   } catch (e) {
-    thrown = true;
+    thrown = true
   }
 
-  t.ok(thrown);
-});
+  t.ok(thrown)
+})
